33eaad624ff22b10f1034c1c09267918bcc7c330
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-05-16 15:56:19 +0200

    fix: Join child svg height

--- src/components/Join/Join.stories.tsx
+++ src/components/Join/Join.stories.tsx
@@ -31,3 +31,12 @@ export const JoinedInputAndButton: Story = {
         </>
     }
 }
+
+export const JoinedInputAndIconButton: Story = {
+    args: {
+        children: <>
+            <Input id="input" variant="primary" />
+            <Button variant="primary" square><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg></Button>
+        </>
+    }
+}
